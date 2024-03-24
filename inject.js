fb72b38840817654ab9f78e318ee28dda669b04b
Julian Rosse <julian@helixbass.net>
2024-03-24 10:41:23 -0400

    retry

--- inject.js
+++ inject.js
@@ -1,11 +1,19 @@
 chrome.extension.sendMessage({}, function(response) {
+        let numChecksAfterReady = 0
+        let hasLoadedBackgroundImage = false
 	var readyStateCheckInterval = setInterval(function() {
 		if (document.readyState === "complete") {
-			clearInterval(readyStateCheckInterval);
+                        numChecksAfterReady++
+                        if (numChecksAfterReady > 100 || hasLoadedBackgroundImage) {
+                          clearInterval(readyStateCheckInterval);
+                        }
 
 			// ----------------------------------------------------------
 			// This part of the script triggers when page is done loading
 			let trello_root_element = document.getElementById('trello-root');
+                        if (!!trello_root_element.style.backgroundImage) {
+                          hasLoadedBackgroundImage = true
+                        }
 			trello_root_element.style.backgroundImage = null;
 			trello_root_element.style.backgroundColor = "rgb(137, 96, 158)";
 			// ----------------------------------------------------------
